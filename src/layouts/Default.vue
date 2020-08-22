14d01485bddbbcaa85d0b3d437399a542be6efcb
Nate Spilman <nate.spilman@gmail.com>
2020-08-22 15:00:59 -0700

    it's alive. testing Netlify prebuild hooks

---
+++ src/layouts/Default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="layout">
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+#main {
+  padding-top:1rem;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,171 @@
+<template>
+  <Layout>
+<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="index.html">Everyone Plays the Same Song</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="generic.html">Generic</a></li>
+								<li><a href="elements.html">Elements</a></li>
+								<li><a href="#">Log In</a></li>
+								<li><a href="#">Sign Up</a></li>
+							</ul>
+						</div>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+						<!-- One -->
+							<section id="one" class="features">
+								<header class="major" id="one-header">
+									<h2>Everyone Plays the Same Song</h2>
+									<p>A FUN AND EDUCATIONAL COMMUNITY COVERS PROJECT</p>
+								</header>
+								<div class="content">
+									<section class="feature">
+										<h3>How it Works</h3>
+                    All participants 
+										<ul>
+                      <li>
+                        Sign up by submitting a song they'd like to cover.
+                      </li>
+                      <li>
+                        Vote on the song they want to cover. The song with the highest average is the song to cover.
+                      </li>
+                        <li>
+                        Record and submit a cover of the song by the submission deadline. 
+                      </li>
+                       <li>
+                        Celebrate at a virtual listening party with all other participants.
+                      </li>
+										</ul>
+									</section>
+									<section class="feature">
+										<h3>The Numbers</h3>
+                    So far and counting
+                    <ul>
+                      <li>3 Rounds</li>
+                      <li>20 unique participants</li>
+                      <li>From 7 different cities</li>
+                      <li>In 3 different time zones</li>
+                    </ul>
+									</section>
+									<section class="feature">
+										<h3>How to Get Involved</h3>
+										<p>Signups will soon be open for round 4!</p>
+									</section>
+								</div>
+							</section>
+
+						<!-- Two -->
+							<section id="two" class="spotlight">
+								<div class="image"><img src="../assets/images/pic01.jpg" alt="" /></div><div class="content">
+									<h2>Volutpat ante libero</h2>
+									<p>Praesent egestas quam at lorem imperdiet lobortis. Mauris condimentum et euismod ipsum, at ullamcorper libero dolor auctor sit amet. Proin vulputate amet sem ut tempus. Donec quis ante viverra, suscipa facilisis at, vestibulum id urna. Lorem ipsum dolor sit amet sollicitudin.</p>
+								</div>
+							</section>
+
+						<!-- Three -->
+							<section id="three" class="spotlight alt">
+								<div class="image"><img src="../assets/images/pic02.jpg" alt="" /></div><div class="content">
+									<h2>Elit auctor tempus</h2>
+									<p>Praesent egestas quam at lorem imperdiet lobortis. Mauris condimentum et euismod ipsum, at ullamcorper libero dolor auctor sit amet. Proin vulputate amet sem ut tempus. Donec quis ante viverra, suscipa facilisis at, vestibulum id urna. Lorem ipsum dolor sit amet sollicitudin.</p>
+								</div>
+							</section>
+
+						<!-- Four -->
+							<section id="four" class="spotlight">
+								<div class="image"><img src="../assets/images/pic03.jpg" alt="" /></div><div class="content">
+									<h2>Porta vestibulum</h2>
+									<p>Praesent egestas quam at lorem imperdiet lobortis. Mauris condimentum et euismod ipsum, at ullamcorper libero dolor auctor sit amet. Proin vulputate amet sem ut tempus. Donec quis ante viverra, suscipa facilisis at, vestibulum id urna. Lorem ipsum dolor sit amet sollicitudin.</p>
+								</div>
+							</section>
+
+						<!-- Five -->
+							<section id="five" class="cta">
+								<header>
+									<h2>Sed euismod nullam</h2>
+									<p>Odio mattis nec velit eu, luctus dictum diam. Quis<br />
+									tempus que ornare purus a bibendum ultricies</p>
+								</header>
+								<ul class="actions special">
+									<li><a href="#" class="button large">Get Started</a></li>
+								</ul>
+							</section>
+
+					</div>
+
+				<!-- Footer -->
+					<section id="footer">
+						<div class="inner">
+							<section class="about">
+								<h3>Proin sed ultricies</h3>
+								<p>Praesent egestas quam at lorem imperdiet lobortis. Mauris condimentum et euismod ipsum, at ullamcorper libero dolor auctor sit amet. Proin vulputate.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">Learn More</a></li>
+								</ul>
+							</section>
+							<section>
+								<h3>Get in Touch</h3>
+								<ul class="contact">
+									<li class="icon solid fa-phone">(000) 000-0000</li>
+									<li class="icon solid fa-envelope"><a href="#">information@untitled.tld</a></li>
+									<li class="icon brands fa-twitter"><a href="#">@untitled-tld</a></li>
+									<li class="icon brands fa-facebook-f"><a href="#">facebook.com/untitled</a></li>
+								</ul>
+							</section>
+							<section>
+								<h3>Mailing Address</h3>
+								<p>Untitled Corp<br />
+								1234 Fictional Road<br />
+								Suite 5432<br />
+								Nashville, TN 00000<br />
+								USA</p>
+							</section>
+						</div>
+						<div class="copyright">
+							<p>Copyright &copy; Untitled Corp. All rights reserved.</p>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<!-- <script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script> -->
+
+	</body>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+#one-header{
+  background-image: url("../assets/images/section_one_bg.jpg");
+  background-size: 100%;
+}
+</style>
