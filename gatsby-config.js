b6667d7131cfc52c05f106c5b188dd132ecac751
Nate Spilman <nate.spilman@gmail.com>
2021-01-01 21:37:13 -0800

    updating favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Everyone Plays the Same Song`,
+        short_name: `EPTSS`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
